3894d37fc7f51430abab3b61b3f0534d89deece9
zhangjie <451117872@qq.com>
2018-01-14 23:35:38 +0800

    1

--- src/com/arithmetic/test/TestMaths.java
+++ src/com/arithmetic/test/TestMaths.java
@@ -1,5 +1,37 @@
 package com.arithmetic.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.arithmetic.entry.MajorityEntry;
+import com.arithmetic.exercise.Maths;
+
 public class TestMaths {
 	
+	
+	
+	@Test
+	public void majorityNumberHalf()
+	{
+		System.out.println("--majorityNumberHalf------------------");
+		int[] arr = new int[]{1,2,2,1,5,3,1,2,1,1,1};
+		int majorityNumber = Maths.majorityNumberHalf(arr);
+		System.out.println(Arrays.toString(arr));
+		System.out.println("majorityNumber:"+majorityNumber);
+		System.out.println("");
+	}
+
+	
+	@Test
+	public void majorityNumberK()
+	{
+		System.out.println("--majorityNumberK------------------");
+		int[] arr = new int[]{1,2,2,3,5,2,3,2,1,3};
+		int majorityNumber = Maths.majorityNumberK(arr,3);
+		System.out.println(Arrays.toString(arr));
+		System.out.println("majorityNumber:"+majorityNumber);
+		System.out.println("");
+	}
 }
