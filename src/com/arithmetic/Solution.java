16e5c85696caa570dde2e53190684ebc9e405ef3
zhangjie <451117872@qq.com>
2018-02-01 21:05:52 +0800

    1

--- src/com/arithmetic/Solution.java
+++ src/com/arithmetic/Solution.java
@@ -7,56 +7,29 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class Solution {
-		public static void main(String[] srt)
-		{
-				String s = "abbaaaaaa";
-				System.out.println(minCut(s));
-		}
-		
-	    public static  int minCut(String s) {
-	    		if( s == null || s.length() < 2) return 0;
-	    		int[][] dp = new int[s.length()][s.length()];
-	    		int n = s.length();
-	    		for(int i=n-1; i>=0; i--)
-	    		{
-	    			for(int j=i+1; j<n; j++)
-	    			{
-	    				String subString = j == n ? s.substring(i) : s.substring(i, j+1);
-	    				if(!isPalindromic(subString))
-	    				{
-	    					dp[i][j] = Integer.MAX_VALUE;
-	    					for(int k=i; k<j; k++)
-	    					{
-	    						dp[i][j] = Math.min(dp[i][j],dp[i][k] + dp[k+1][j]+1);
-	    					}
-	    				}
-	    			}
-	    		}
-	    		for(int i=0; i<dp.length; i++)
-	    		{
-	    			System.out.println(Arrays.toString(dp[i]));
-	    		}
-	    		return dp[0][s.length()-1];
-	    }
-	    
-	    public static  boolean isPalindromic(String sub) {
-			if(sub == null || "".equals(sub))
-				return false;
-			if(sub.length() == 1)
-				return true;
-			int start = 0;
-			int end = sub.length()-1;
-			while(start < end)
-			{
-				if(sub.charAt(start) != sub.charAt(end))
-				{
-					return false;
-				}
-				start ++;
-				end --;
-			}
-			return true;
-		}
+	public static void main(String[] srt)
+	{
+			String s = "abbaaaaaa";
+	}
+	
+	 public int removeElement(int[] nums, int val) {
+		 if(nums == null || nums.length < 1) return 0;
+		 int k = nums.length-1;
+		 int start = 0;
+		 int len = nums.length;
+		 while(start < len && start < k)
+		 {
+			 if(nums[start] == val)
+			 {
+				 nums[start] = nums[k];
+				 nums[k] = val;
+				 k --;
+			 }
+			 start ++;
+		 }
+		 
+	     return k+1;   
+	}
 
 		
 }
